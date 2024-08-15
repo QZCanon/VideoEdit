5b6b072ded108265eae7348234588d492c8d2c47
QzGithub0617 <274471687@qq.com>
2024-08-15 21:03:29 +0800

    feat: play video, use qt media

--- mainwindow.h
+++ mainwindow.h
@@ -4,11 +4,6 @@
 #include <QMainWindow>
 #include <memory>
 
-#include <QLabel>
-#include <QTimer>
-#include <QImage>
-#include <QPixmap>
-
 #include <QWidget>
 #include <QMediaPlayer>
 #include <QVideoWidget>
@@ -41,17 +36,17 @@ private:
     std::shared_ptr<FitParse> fitParse{nullptr};
 
     // VideoPlayer *player{nullptr};
-    QVideoWidget *m_pPlayerWidget;// 视频显示组件
-    QMediaPlayer * m_pPlayer;//媒体播放器类
-
+    QVideoWidget* m_pPlayerWidget{nullptr}; // 视频显示组件
+    QAudioOutput* m_audio{nullptr};         // 音频播放器
+    QMediaPlayer* m_pPlayer;                // 媒体播放器类
+    bool          m_bReLoad;                // 已经载入还没设置进度条最大值
+    float         m_scale{0.0};
 
 public slots:
-    void    OnSetMediaFile(void);//载入
-    void    OnSlider(qint64); // 信号内容的位置已更改为位置，以毫秒表示，进度条也要变
-    void    OnDurationChanged(qint64);// 信号表示内容的持续时间已更改为时长，以毫秒表示，进度条 的最大值和最小值
-    // void    OnStateChanged(QMediaPlayer::State);// Player对象的状态已经改变
-
-
+    void OnSetMediaFile(void);                           // 选择文件导入
+    void OnDurationChanged(qint64);                      // 信号表示内容的持续时间已更改为时长，以毫秒表示，进度条 的最大值和最小值
+    void OnPositionChanged(qint64);                      // 当前播放位置
+    void OnMediaStateChanged(QMediaPlayer::MediaStatus); // Player对象的状态已经改变
 
 };
 #endif // MAINWINDOW_H
